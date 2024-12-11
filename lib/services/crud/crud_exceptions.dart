e60a68ebcf699603464970cc150b0b5e99665f81
hamdi-ab <hamdiabdulfetah22@gmail.com>
2024-12-11 22:20:06 +0300

    step 11

---
+++ lib/services/crud/crud_exceptions.dart
@@ -0,0 +1,17 @@
+class DatabaseAlreadyOpenException implements Exception {}
+
+class UnableToGetDocumentDirectory implements Exception {}
+
+class DatabaseIsNotOpen implements Exception {}
+
+class CouldNotDeleteUser implements Exception {}
+
+class CouldNotFoundUser implements Exception {}
+
+class CouldNotDeleteNote implements Exception {}
+
+class CouldNotFoundNote implements Exception {}
+
+class UserAlreadyExists implements Exception {}
+
+class CouldNotUpdateNote implements Exception {}
