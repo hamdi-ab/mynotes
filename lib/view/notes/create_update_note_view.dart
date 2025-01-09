87ff29fbf0aa7f05cc48dd6a5dcab54a89688d9f
hamdi-ab <hamdiabdulfetah22@gmail.com>
2025-01-09 20:06:02 +0300

    step-21

--- lib/view/notes/create_update_note_view.dart
+++ lib/view/notes/create_update_note_view.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:new1/services/auth/auth_service.dart';
+import 'package:new1/utilities/dialogs/cannot_share_empty_note_dialog.dart';
 import 'package:new1/utilities/generics/get_arguments.dart';
 import 'package:new1/services/cloud/cloud_note.dart';
 import 'package:new1/services/cloud/firebase_cloud_storage.dart';
+import 'package:share_plus/share_plus.dart';
 
 class NewNoteView extends StatefulWidget {
   const NewNoteView({super.key});
@@ -32,16 +34,15 @@ class _NewNoteViewState extends State<NewNoteView> {
     await _notesService.updateNote(documentId: note.documentId, text: text);
   }
 
-  void _setUpTextControllerListener(){
+  void _setUpTextControllerListener() {
     _textController.removeListener(_textControllerListener);
     _textController.addListener(_textControllerListener);
   }
 
   Future<CloudNote> createOrGetExistingNote(BuildContext context) async {
-
     final widgetNote = context.getArgument<CloudNote>();
 
-    if(widgetNote != null){
+    if (widgetNote != null) {
       _note = widgetNote;
       _textController.text = widgetNote.text;
       return widgetNote;
@@ -55,7 +56,7 @@ class _NewNoteViewState extends State<NewNoteView> {
     //final email = currentUser.email;
     // final owner = await _notesService.getUser(email: email); thia was for the local database
     final userId = currentUser.id;
-    final newNote=  await _notesService.createNewNote(ownerUserId: userId);
+    final newNote = await _notesService.createNewNote(ownerUserId: userId);
     _note = newNote;
     return newNote;
   }
@@ -87,12 +88,25 @@ class _NewNoteViewState extends State<NewNoteView> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () async {
+              final text = _textController.text;
+              if(_note == null || text.isEmpty){
+                await showCannotShareEmptyNoteDialog(context);
+              }else{
+                Share.share(text);
+              }
+            },
+            icon: const Icon(Icons.share),
+          )
+        ],
         title: const Text('New Note'),
       ),
       body: FutureBuilder(
         future: createOrGetExistingNote(context),
-        builder:(context, snapshot){
-          switch(snapshot.connectionState){
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
             case ConnectionState.done:
               _setUpTextControllerListener();
               return TextField(
@@ -100,13 +114,12 @@ class _NewNoteViewState extends State<NewNoteView> {
                 keyboardType: TextInputType.multiline,
                 maxLines: null,
                 decoration: const InputDecoration(
-                  hintText: 'Start typing your note....'
-                ),
+                    hintText: 'Start typing your note....'),
               );
             default:
               return const CircularProgressIndicator();
           }
-      },
+        },
       ),
     );
   }
